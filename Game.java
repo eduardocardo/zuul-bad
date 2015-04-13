0e883d844b89a355e5238f6f59f63037dd64a177
Eduardo <eduardocardomartinez@gmail.com>
2015-04-13 09:58:44 +0200

    Añadido nuevo comando eat a la clase CommandWords e implementado en clase Game

--- Game.java
+++ Game.java
@@ -131,6 +131,10 @@ public class Game
         {
            System.out.println(currentRoom.getLongDescription());
         }
+        else if(commandWord.equals("eat"))
+        {
+            System.out.println("You have eaten now and you are not hungry any more");
+        }
 
         return wantToQuit;
     }
