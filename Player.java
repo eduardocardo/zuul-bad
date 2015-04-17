6842462efc1dbccb45327c96b1081d3dd9fc9a33
Eduardo <eduardocardomartinez@gmail.com>
2015-04-17 13:37:50 +0200

    Creados metodos en clase player eat,look,backRoom,goRoom

--- Player.java
+++ Player.java
@@ -29,5 +29,60 @@ public class Player
         currentRoom = room;
     }
     
+    /**
+     * Metodo que hace que el jugador coma
+     */
+    public void eat()
+    {
+        System.out.println("You have eaten now and you are not hungry any more");
+    }
     
+    /**
+     * Metodo que da al jugador una informacion sobre la habitacion en la que 
+     * se encuentra
+     */
+    public void look()
+    {
+        System.out.println(currentRoom.getLongDescription());
+    }
+    
+    /**
+     * Metodo que desplaza al jugador por las habitaciones por las salidas disponibles
+     * @param direccion es la direccion a la que se desplaza el jugador
+     */
+    
+    public void goRoom(String direccion)
+    {
+        //objeto tipo Room que representa la habitacion a la que se desplaza
+        Room nextRoom = null;
+        //se guarda la habitacion a la que se desplazaria segun la direccion del parametro
+        nextRoom = currentRoom.getExit(direccion);
+
+        if (nextRoom == null) {
+            
+            System.out.println("There is no door!");
+        }
+        else {
+            stacks.push(currentRoom);
+            currentRoom = nextRoom;
+            look();
+            
+        }
+    }
+    
+     /**
+     * Metodo que vuelve a la habitacion anterior
+     */
+    private void backRoom()
+    {
+        if(!stacks.empty())
+        {
+            currentRoom =stacks.pop();
+            look();
+        }
+        else
+        {
+            System.out.println("No puedes volver a atras");
+        }
+    }
 }
